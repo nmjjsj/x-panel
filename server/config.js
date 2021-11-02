ed9bd8164490231d3371c2993cc1a99a47c904cb
RoodyCode <you@example.com>
2021-11-03 00:21:20 +0100

    fixed node version

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   webPort: process.env.PORT || 3000,
-  password: process.env.PASSWORD || '473',
+  password: process.env.PASSWORD || '123',
   defaultPath: process.env.DEFAULT_PATH || '/app'
 }
